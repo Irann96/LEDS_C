04057b8af8c7fbe9adf7e95ddfbe2525adbdcb74
Iran Melchor <iraann.96@gmail.com>
2021-04-29 04:52:46 +0000

    LEDc.c

---
+++ LEDc.c
@@ -0,0 +1,51 @@
+//Code to control the LED3 usr by arguments
+//27/04/21
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define LED3 "/sys/class/leds/beaglebone:green:usr3"
+  
+      //Prototypes functions:
+void WriteLED(char filename[], char value[]);
+void RemoveTrigger();
+
+int main(int argc, char* argv[]) {
+	if(argc!=2){
+		printf("Error wrong number of arguments!!\n");
+		return 2;
+	}
+	printf("Starting the script....\n");
+	//comparing arguments
+	if(strcmp(argv[1], "on")==0){
+		printf("LED on\n");
+		RemoveTrigger();
+		WriteLED( "/brightness", "1");  //argumentos
+	}
+
+	else if(strcmp(argv[1], "off")==0){
+		printf("LED off\n");
+		RemoveTrigger();
+		WriteLED("/brightness", "0");
+	}
+printf("Led Aplication Done \n");	
+	return 0;
+
+}
+
+//Complete functions:
+void WriteLED(char filename[], char value[]){
+	FILE* fp; //CORRESPONDE AL APUNTADOR que debo investigar
+	char fullFile[100];
+	sprintf(fullFile, LED3 "%s", filename);  //LA S, XK CORRESPONDE AL TIPO DE CARACTER
+	fp = fopen(fullFile, "w+");
+	fprintf(fp, "%s", value);
+	fclose(fp);
+}
+
+void RemoveTrigger(){
+	WriteLED("/trigger", "none");
+}
+
+
